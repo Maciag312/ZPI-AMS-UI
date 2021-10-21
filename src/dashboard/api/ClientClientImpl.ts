361f545dd0b7639a47e34637647135c78fb60e59
Bartłomiej Chmiel <bachm44@gmail.com>
2021-10-21 20:31:51 +0200

    Fix dependencies

--- src/dashboard/api/ClientClientImpl.ts
+++ src/dashboard/api/ClientClientImpl.ts
@@ -1,20 +1,19 @@
 import axios, { AxiosResponse } from "axios";
+import Client from "../../common/Client";
 import { host } from "./AuthorizationServerConfig";
 import { ClientClient } from "./ClientClient";
-import Client from "../../common/Client";
 import URICreator from "./URICreator";
 
 class ClientClientImpl implements ClientClient {
-    host: string;
+  host: string;
 
-    constructor(host: string) {
-        this.host = host;
-    }
+  constructor(host: string) {
+    this.host = host;
+  }
 
-    add(client: Client): Promise<AxiosResponse<any, any>> {
-        return axios.post(URICreator.addClient(this.host), client);
-    }
-
+  add(client: Client): Promise<AxiosResponse<any>> {
+    return axios.post(URICreator.addClient(this.host), client);
+  }
 }
 
 export default new ClientClientImpl(host);
